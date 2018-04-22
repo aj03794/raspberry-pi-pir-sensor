08617c03a78ddf066d69d3f40988a1e46099ae7c
aj03794 <adamjohnston151@yahoo.com>
2018-04-22 21:09:29 +0000

    Removing setInterval for testing

--- src/motion-sensor/index.js
+++ src/motion-sensor/index.js
@@ -1,14 +1,14 @@
 let motion = false
 
 export const monitorMotionSensor = ({ sendMsg }) => {
-	setInterval(() => {
-		motion = !motion
-		if(motion) {
+	// setInterval(() => {
+	// 	motion = !motion
+		// if(motion) {
 			console.log('Motion true', motion)
 			return sendMsg({ motion: true })
-		} else {
-			console.log('Motion false', motion)
-			return sendMsg({ motion: false })
-		}
-	}, 5000)
+	// 	} else {
+	// 		console.log('Motion false', motion)
+	// 		return sendMsg({ motion: false })
+	// 	}
+	// }, 5000)
 }
