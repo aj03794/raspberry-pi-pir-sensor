4992d678ea061977c86e10b075c98207fc2617a3
aj03794 <adamjohnston151@yahoo.com>
2018-06-03 13:57:02 -0400

    moving code inside of try/catch block

--- src/index.js
+++ src/index.js
@@ -6,13 +6,9 @@ console.log('platform', platform())
 
 const { publish, subscribe } = redis()
 
-// console.log('publish', publish)
-
 const raspi = require('../package.json').dependencies['raspi-io']
 const five = require('../package.json').dependencies['johnny-five']
 
-// console.log('raspi', raspi)
-// console.log('five', five)
 
 monitorMotionSensor({
 	raspi,
